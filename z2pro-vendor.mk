840f83b16388d323e813dda70ff9978d0fdbcc49
switchgott <switchgott@yahoo.de>
2017-01-19 15:10:50 +0100

    Init Zuk Z2 Pro CM13 Vendor

---
+++ z2pro-vendor.mk
@@ -0,0 +1,8 @@
+# Copyright (C) 2016 The CyanogenMod Project
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zuk/z2pro/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zuk/z2pro/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zuk/z2pro/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zuk/z2pro/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zuk/z2pro/proprietary/lib64,system/lib64)
+-include vendor/extra/devices.mk
